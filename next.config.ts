e8535e49df892a5ebd7e1f26e99beb7366f8287e
Nate-Workspace <nathanisrael.jobs@gmail.com>
2025-05-31 18:14:39 +0300

    Updating config and Image - Last task

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+   images: {
+    domains: ['example.com'], // allow this domain
+  },
 };
 
 export default nextConfig;
